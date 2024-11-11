9b03de43cd06721d01f115ad17c94061c71e63a3
Marco-Contreras-Progra <m.contreras28@ufromail.cl>
2024-11-11 13:32:53 -0300

    Clases generadas ejercicio

---
+++ demo/src/main/java/com/example/Equipo.java
@@ -0,0 +1,13 @@
+public class Equipo {
+	private String _nombre;
+	private Jugador _jugadores;
+	private Jugador _capitan;
+
+	public void agregarJugador() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void asignarCapitan() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ demo/src/main/java/com/example/Jugador.java
@@ -0,0 +1,14 @@
+public class Jugador {
+	private String _nombre;
+	private String _rut;
+	private Equipo _equipo;
+	private boolean _esCapitan;
+
+	public void asignarEquipo() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void establecerCapitan() {
+		throw new UnsupportedOperationException();
+	}
+}
